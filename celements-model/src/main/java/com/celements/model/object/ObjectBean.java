0f3b14891934eaaa967b8ec663f431d6e5810399
Fabian Pichler <fabian.pichler@synventis.com>
2022-01-14 11:47:32 +0100

    formatting and refactorings

--- celements-model/src/main/java/com/celements/model/object/ObjectBean.java
+++ celements-model/src/main/java/com/celements/model/object/ObjectBean.java
@@ -3,7 +3,6 @@ package com.celements.model.object;
 import java.util.Objects;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import javax.validation.constraints.NotEmpty;
 
 import org.xwiki.model.reference.ClassReference;
 import org.xwiki.model.reference.DocumentReference;
@@ -12,8 +11,8 @@ import org.xwiki.model.reference.DocumentReference;
 public class ObjectBean {
 
   private DocumentReference documentReference;
-  private Integer number;
   private ClassReference classReference;
+  private Integer number;
 
   public DocumentReference getDocumentReference() {
     return documentReference;
@@ -21,14 +20,6 @@ public class ObjectBean {
 
   public void setDocumentReference(DocumentReference documentReference) {
     this.documentReference = documentReference;
-  }
-
-  public Integer getNumber() {
-    return number;
-  }
-
-  public void setNumber(Integer number) {
-    this.number = number;
   }
 
   public ClassReference getClassReference() {
@@ -39,23 +30,30 @@ public class ObjectBean {
     this.classReference = classReference;
   }
 
+  public Integer getNumber() {
+    return number;
+  }
+
+  public void setNumber(Integer number) {
+    this.number = number;
+  }
+
   @Override
   public boolean equals(Object obj) {
     return (obj instanceof ObjectBean)
         && Objects.equals(((ObjectBean) obj).documentReference, this.documentReference)
-        && Objects.equals(((ObjectBean) obj).number, this.number)
-        && Objects.equals(((ObjectBean) obj).classReference, this.classReference);
+        && Objects.equals(((ObjectBean) obj).classReference, this.classReference)
+        && Objects.equals(((ObjectBean) obj).number, this.number);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(documentReference, number, classReference);
+    return Objects.hash(documentReference, classReference, number);
   }
 
   @Override
-  @NotEmpty
   public String toString() {
-    return "docRef [" + documentReference + "], classReference [" + classReference
-        + "] from objNum [" + number + "]";
+    return "ObjectBean [documentReference=" + documentReference + ", classReference="
+        + classReference + ", number=" + number + "]";
   }
 }
--- celements-model/src/test/java/com/celements/model/object/ObjectBeanTest.java
+++ celements-model/src/test/java/com/celements/model/object/ObjectBeanTest.java
@@ -27,10 +27,11 @@ public class ObjectBeanTest extends AbstractComponentTest {
   }
 
   @Test
-  public void test_setNumber() {
-    Integer num = 123;
-    objectBean.setNumber(num);
-    assertEquals("Number field is needed for bean", num, objectBean.getNumber());
+  public void test_setDocumentReference() {
+    DocumentReference docRef = new DocumentReference("wikiName", "space", "classname");
+    objectBean.setDocumentReference(docRef);
+    assertEquals("ClassReference field is needed for bean", docRef,
+        objectBean.getDocumentReference());
   }
 
   @Test
@@ -42,24 +43,23 @@ public class ObjectBeanTest extends AbstractComponentTest {
   }
 
   @Test
-  public void test_setDocumentReference() {
-    DocumentReference docRef = new DocumentReference("wikiName", "space", "classname");
-    objectBean.setDocumentReference(docRef);
-    assertEquals("ClassReference field is needed for bean", docRef,
-        objectBean.getDocumentReference());
+  public void test_setNumber() {
+    Integer num = 123;
+    objectBean.setNumber(num);
+    assertEquals("Number field is needed for bean", num, objectBean.getNumber());
   }
 
   @Test
   public void test_equals_equals() {
     DocumentReference docRef = new DocumentReference("wikiName", "space", "document");
-    Integer objNum = 2;
+    Integer number = 2;
     objectBean.setDocumentReference(docRef);
     objectBean.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean.setNumber(objNum);
+    objectBean.setNumber(number);
     ObjectBean objectBean2 = new ObjectBean();
     objectBean2.setDocumentReference(docRef);
     objectBean2.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean2.setNumber(objNum);
+    objectBean2.setNumber(number);
     assertEquals(objectBean, objectBean2);
     assertEquals(objectBean.hashCode(), objectBean2.hashCode());
   }
@@ -67,15 +67,15 @@ public class ObjectBeanTest extends AbstractComponentTest {
   @Test
   public void test_equals_notEquals_docRef() {
     DocumentReference docRef = new DocumentReference("wikiName", "space", "document");
-    Integer objNum = 2;
+    Integer number = 2;
     objectBean.setDocumentReference(docRef);
     objectBean.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean.setNumber(objNum);
+    objectBean.setNumber(number);
     ObjectBean objectBean2 = new ObjectBean();
     DocumentReference docRef2 = new DocumentReference("wikiName", "space", "document2");
     objectBean2.setDocumentReference(docRef2);
     objectBean2.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean2.setNumber(objNum);
+    objectBean2.setNumber(number);
     assertNotEquals(objectBean, objectBean2);
     assertNotEquals(objectBean.hashCode(), objectBean2.hashCode());
   }
@@ -83,30 +83,30 @@ public class ObjectBeanTest extends AbstractComponentTest {
   @Test
   public void test_equals_notEquals_classRef() {
     DocumentReference docRef = new DocumentReference("wikiName", "space", "document");
-    Integer objNum = 2;
+    Integer number = 2;
     objectBean.setDocumentReference(docRef);
     objectBean.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean.setNumber(objNum);
+    objectBean.setNumber(number);
     ObjectBean objectBean2 = new ObjectBean();
     objectBean2.setDocumentReference(docRef);
     objectBean2.setClassReference(new ClassReference("spaceTest", "ClassTest2"));
-    objectBean2.setNumber(objNum);
+    objectBean2.setNumber(number);
     assertNotEquals(objectBean, objectBean2);
     assertNotEquals(objectBean.hashCode(), objectBean2.hashCode());
   }
 
   @Test
-  public void test_equals_notEquals_objNum() {
+  public void test_equals_notEquals_number() {
     DocumentReference docRef = new DocumentReference("wikiName", "space", "document");
-    Integer objNum = 2;
+    Integer number = 2;
     objectBean.setDocumentReference(docRef);
     objectBean.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean.setNumber(objNum);
+    objectBean.setNumber(number);
     ObjectBean objectBean2 = new ObjectBean();
     objectBean2.setDocumentReference(docRef);
     objectBean2.setClassReference(TestClassDefinition.CLASS_REF);
-    Integer objNum2 = 4;
-    objectBean2.setNumber(objNum2);
+    Integer number2 = 4;
+    objectBean2.setNumber(number2);
     assertNotEquals(objectBean, objectBean2);
     assertNotEquals(objectBean.hashCode(), objectBean2.hashCode());
   }
@@ -114,25 +114,26 @@ public class ObjectBeanTest extends AbstractComponentTest {
   @Test
   public void test_toString() {
     DocumentReference docRef = new DocumentReference("wikiName", "space", "document");
-    Integer objNum = 2;
+    ClassReference classReference = TestClassDefinition.CLASS_REF;
+    Integer number = 2;
     objectBean.setDocumentReference(docRef);
-    objectBean.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean.setNumber(objNum);
-    assertEquals("docRef [" + docRef + "], classReference [" + TestClassDefinition.CLASS_REF
-        + "] from objNum [" + objNum + "]", objectBean.toString());
+    objectBean.setClassReference(classReference);
+    objectBean.setNumber(number);
+    assertEquals("ObjectBean [documentReference=" + docRef + ", classReference=" + classReference
+        + ", number=" + number + "]", objectBean.toString());
   }
 
   @Test
   public void test_ObjectBean_bean() {
     DocumentReference docRef = new DocumentReference("wikiName", "space", "document");
-    Integer objNum = 2;
+    Integer number = 2;
     objectBean.setDocumentReference(docRef);
     objectBean.setClassReference(TestClassDefinition.CLASS_REF);
-    objectBean.setNumber(objNum);
+    objectBean.setNumber(number);
     BaseObject objectBeanObj = new BaseObject();
     objectBeanObj.setXClassReference(TestClassDefinition.CLASS_REF);
     objectBeanObj.setDocumentReference(docRef);
-    objectBeanObj.setNumber(objNum);
+    objectBeanObj.setNumber(number);
     try {
       ObjectBean objectBean2 = createTestClassConverter().apply(objectBeanObj);
       assertEquals(objectBean, objectBean2);
