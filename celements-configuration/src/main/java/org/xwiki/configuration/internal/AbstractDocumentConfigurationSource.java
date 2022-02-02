b70569fd15f6ecf755242e9fe0a3d312c9fb68fb
Marc Sladek <marc@sladek.me>
2022-02-02 16:02:46 +0100

    create celements-configuration

---
+++ celements-configuration/src/main/java/org/xwiki/configuration/internal/AbstractDocumentConfigurationSource.java
@@ -0,0 +1,187 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.configuration.internal;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import org.xwiki.bridge.DocumentAccessBridge;
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.configuration.ConfigurationSource;
+import org.xwiki.model.reference.DocumentReference;
+
+/**
+ * Common features for all Document sources (ie configuration data coming from wiki pages).
+ *
+ * @version $Id$
+ * @since 2.0M2
+ */
+public abstract class AbstractDocumentConfigurationSource implements ConfigurationSource {
+
+  /**
+   * @see #getDocumentAccessBridge()
+   */
+  @Requirement
+  private DocumentAccessBridge documentAccessBridge;
+
+  /**
+   * @return the document reference of the document containing an XWiki Object with configuration
+   *         data or null
+   *         if there no such document in which case this configuration source will be skipped
+   */
+  protected abstract DocumentReference getDocumentReference();
+
+  /**
+   * @return the XWiki Class reference of the XWiki Object containing the configuration properties
+   */
+  protected abstract DocumentReference getClassReference();
+
+  /**
+   * @return the bridge used to access Object properties
+   */
+  protected DocumentAccessBridge getDocumentAccessBridge() {
+    return this.documentAccessBridge;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see ConfigurationSource#containsKey(String)
+   */
+  @Override
+  public boolean containsKey(String key) {
+    DocumentReference documentReference = getFailsafeDocumentReference();
+    DocumentReference classReference = getFailsafeClassReference();
+    return ((documentReference != null) && (classReference != null))
+        && (getDocumentAccessBridge().getProperty(documentReference, classReference, key) != null);
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see ConfigurationSource#getKeys()
+   */
+  @Override
+  public List<String> getKeys() {
+    return null;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see ConfigurationSource#getProperty(String, Object)
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T> T getProperty(String key, T defaultValue) {
+    return getProperty(key, defaultValue, (Class<T>) defaultValue.getClass());
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see ConfigurationSource#getProperty(String, Class)
+   */
+  @Override
+  public <T> T getProperty(String key, Class<T> valueClass) {
+    T result = null;
+
+    DocumentReference documentReference = getFailsafeDocumentReference();
+    DocumentReference classReference = getFailsafeClassReference();
+    if ((documentReference != null) && (classReference != null)) {
+      result = (T) getDocumentAccessBridge().getProperty(documentReference, classReference, key);
+
+      // Make sure we don't return null values for List and Properties (they must return empty
+      // elements
+      // when using the typed API).
+      if (result == null) {
+        if (List.class.getName().equals(valueClass.getName())) {
+          result = (T) Collections.emptyList();
+        } else if (Properties.class.getName().equals(valueClass.getName())) {
+          result = (T) new Properties();
+        }
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see ConfigurationSource#getProperty(String)
+   */
+  @Override
+  public <T> T getProperty(String key) {
+    T result = null;
+    DocumentReference documentReference = getFailsafeDocumentReference();
+    DocumentReference classReference = getFailsafeClassReference();
+    if ((documentReference != null) && (classReference != null)) {
+      result = (T) getDocumentAccessBridge().getProperty(documentReference, classReference, key);
+    }
+
+    return result;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see ConfigurationSource#isEmpty()
+   */
+  @Override
+  public boolean isEmpty() {
+    return getKeys().isEmpty();
+  }
+
+  private <T> T getProperty(String key, T defaultValue, Class<T> valueClass) {
+    T result = getProperty(key, valueClass);
+    if (result == null) {
+      result = defaultValue;
+    }
+    return result;
+  }
+
+  private DocumentReference getFailsafeDocumentReference() {
+    DocumentReference documentReference;
+    try {
+      documentReference = getDocumentReference();
+    } catch (Exception e) {
+      // We verify that no error has happened and if one happened then we skip this configuration
+      // source. This
+      // ensures the system will continue to work even if this source has a problem.
+      documentReference = null;
+    }
+    return documentReference;
+  }
+
+  private DocumentReference getFailsafeClassReference() {
+    DocumentReference classReference;
+    try {
+      classReference = getDocumentReference();
+    } catch (Exception e) {
+      // We verify that no error has happened and if one happened then we skip this configuration
+      // source. This
+      // ensures the system will continue to work even if this source has a problem.
+      classReference = null;
+    }
+    return classReference;
+  }
+}
---
+++ celements-configuration/src/main/java/org/xwiki/configuration/internal/UserPreferencesConfigurationSource.java
@@ -0,0 +1,51 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.configuration.internal;
+
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
+
+/**
+ * Configuration source taking its data in the User Preferences wiki document
+ * (the user profile page) using data from a XWikiUsers object attached
+ * to that document.
+ *
+ * @version $Id$
+ * @since 2.0M2
+ */
+@Component("user")
+public class UserPreferencesConfigurationSource extends AbstractDocumentConfigurationSource {
+
+  @Override
+  protected DocumentReference getClassReference() {
+    // TODO: Not enabled yet. See #getDocumentReference().
+    return null;
+  }
+
+  @Override
+  protected DocumentReference getDocumentReference() {
+    // TODO: Not enabled yet. In order to enable it we need to make modifications so that
+    // DAB.getCurrentUser() returns a DocumentReference and not a String as otherwise it will create
+    // a stackoverflow (circular dependency): in order to create a DocumentReference we would need
+    // to
+    // use a factory which would need to use this configuration source.
+    return null;
+  }
+}
