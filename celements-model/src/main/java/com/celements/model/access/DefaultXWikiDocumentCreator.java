a0bf7c7a933cb7f85ad8e3d7b94723bf61596dbc
Marc Sladek <marc@sladek.dev>
2022-12-28 14:55:27 +0100

    remove default syntax

--- celements-model/src/main/java/com/celements/model/access/DefaultXWikiDocumentCreator.java
+++ celements-model/src/main/java/com/celements/model/access/DefaultXWikiDocumentCreator.java
@@ -3,21 +3,17 @@ package com.celements.model.access;
 import static com.celements.model.access.IModelAccessFacade.*;
 
 import java.util.Date;
-import java.util.Optional;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
-import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.EntityReference;
 import org.xwiki.model.reference.SpaceReference;
 import org.xwiki.model.reference.WikiReference;
-import org.xwiki.rendering.parser.Parser;
 import org.xwiki.rendering.syntax.Syntax;
 
 import com.celements.model.context.ModelContext;
 import com.celements.model.util.ModelUtils;
-import com.xpn.xwiki.CoreConfiguration;
 import com.xpn.xwiki.doc.XWikiDocument;
 
 @Component
@@ -28,12 +24,6 @@ public class DefaultXWikiDocumentCreator implements XWikiDocumentCreator {
 
   @Requirement
   private ModelUtils modelUtils;
-
-  @Requirement
-  private CoreConfiguration coreCfg;
-
-  @Requirement
-  private ComponentManager componentManager;
 
   @Override
   public XWikiDocument createWithoutDefaults(DocumentReference docRef, String lang) {
@@ -72,7 +62,6 @@ public class DefaultXWikiDocumentCreator implements XWikiDocumentCreator {
     }
     XWikiDocument doc = createWithoutDefaults(docRef, lang);
     doc.setDefaultLanguage(defaultLang);
-    doc.setSyntax(getDefaultSyntax());
     return doc;
   }
 
@@ -91,15 +80,8 @@ public class DefaultXWikiDocumentCreator implements XWikiDocumentCreator {
         .orElseThrow(IllegalStateException::new));
   }
 
-  private Syntax getDefaultSyntax() {
-    return Optional.ofNullable(coreCfg.getDefaultDocumentSyntax())
-        .filter(syntax -> componentManager.hasComponent(Parser.class, syntax.toIdString()))
-        .orElseThrow(IllegalStateException::new);
-  }
-
   @Override
   public XWikiDocument create(DocumentReference docRef) {
     return create(docRef, IModelAccessFacade.DEFAULT_LANG);
   }
-
 }
