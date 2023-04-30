0050413c7781da5bd63fbbbd8b644ef36aff86a5
Marc Sladek <marc@sladek.dev>
2023-04-30 02:05:55 +0200

    remove LogEnabled

--- celements-observation/src/test/java/org/xwiki/observation/ObservationManagerTest.java
+++ celements-observation/src/test/java/org/xwiki/observation/ObservationManagerTest.java
@@ -28,8 +28,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.xwiki.component.logging.Logger;
-import org.xwiki.component.phase.LogEnabled;
 import org.xwiki.observation.event.ActionExecutionEvent;
 import org.xwiki.observation.event.AllEvent;
 import org.xwiki.observation.event.Event;
@@ -233,9 +231,6 @@ public class ObservationManagerTest {
   @Test
   public void testRegisterSameListenerSeveralTimes() {
     final EventListener listener = this.context.mock(EventListener.class);
-    final Logger logger = this.context.mock(Logger.class);
-
-    ((LogEnabled) this.manager).enableLogging(logger);
 
     this.context.checking(new Expectations() {
 
@@ -244,12 +239,6 @@ public class ObservationManagerTest {
         will(returnValue("mylistener"));
         allowing(listener).getEvents();
         will(returnValue(Arrays.asList(AllEvent.ALLEVENT)));
-        // The check is performed here, we verify that a warning is correctly logged
-        oneOf(logger).warn("The [" + listener.getClass().getName()
-            + "] listener has overwritten a previously registered listener ["
-            + listener.getClass().getName() + "] "
-            + "since they both are registered under the same id [mylistener]. In the future consider removing a "
-            + "Listener first if you really want to register it again.");
       }
     });
 
