50db905253ace27eeede2c0023fb6120ef3c02bb
Marc Sladek <marc@sladek.dev>
2023-05-02 20:07:10 +0200

    lowercase wiki

--- celements-xwiki-core/src/test/java/com/xpn/xwiki/doc/XWikiDocumentTest.java
+++ celements-xwiki-core/src/test/java/com/xpn/xwiki/doc/XWikiDocumentTest.java
@@ -62,7 +62,7 @@ import com.xpn.xwiki.web.XWikiMessageTool;
  */
 public class XWikiDocumentTest extends AbstractBridgedXWikiComponentTestCase {
 
-  private static final String DOCWIKI = "Wiki";
+  private static final String DOCWIKI = "wiki";
 
   private static final String DOCSPACE = "Space";
 
