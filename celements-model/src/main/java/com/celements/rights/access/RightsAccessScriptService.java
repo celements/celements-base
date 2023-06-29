03aa046ee9dffa4886b550c8a7f2a80b29fa5855
Chantal Pichler <chantal.pichler@synventis.com>
2023-06-29 17:23:49 +0200

    switch to spring injections and finish new method getGroupRefsForUser

--- celements-model/src/main/java/com/celements/rights/access/RightsAccessScriptService.java
+++ celements-model/src/main/java/com/celements/rights/access/RightsAccessScriptService.java
@@ -1,14 +1,21 @@
 package com.celements.rights.access;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import org.xwiki.component.annotation.Component;
-import org.xwiki.component.annotation.Requirement;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.EntityReference;
 import org.xwiki.script.service.ScriptService;
 
 import com.celements.auth.user.User;
+import com.celements.auth.user.UserInstantiationException;
+import com.celements.auth.user.UserService;
 import com.celements.rights.publication.EPubUnpub;
 import com.celements.rights.publication.IPublicationServiceRole;
 import com.xpn.xwiki.user.api.XWikiRightService;
@@ -17,13 +24,22 @@ import com.xpn.xwiki.user.api.XWikiUser;
 @Component(RightsAccessScriptService.NAME)
 public class RightsAccessScriptService implements ScriptService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(RightsAccessScriptService.class);
+
   public static final String NAME = "rightsAccess";
 
-  @Requirement
-  IPublicationServiceRole pubSrv;
+  private final IPublicationServiceRole pubSrv;
+  private final IRightsAccessFacadeRole rightsAccess;
+  private final UserService userService;
 
-  @Requirement
-  IRightsAccessFacadeRole rightsAccess;
+  @Inject
+  public RightsAccessScriptService(IPublicationServiceRole pubSrv,
+      IRightsAccessFacadeRole rightsAccess, UserService userService) {
+    super();
+    this.pubSrv = pubSrv;
+    this.rightsAccess = rightsAccess;
+    this.userService = userService;
+  }
 
   public EPubUnpub getEPubUnpub(String name) {
     return EPubUnpub.valueOf(name);
@@ -80,10 +96,13 @@ public class RightsAccessScriptService implements ScriptService {
   }
 
   List<DocumentReference> getGroupRefsForUser(DocumentReference userDocRef) {
-    // TODO convert userDocRef in User
-    User user = null;
-
-    return rightsAccess.getGroupRefsForUser(user);
+    try {
+      User user = userService.getUser(userDocRef);
+      return rightsAccess.getGroupRefsForUser(user).collect(Collectors.toList());
+    } catch (UserInstantiationException uie) {
+      LOGGER.error("could not instantiate user for {}", userDocRef, uie);
+      return new ArrayList<>();
+    }
   }
 
 }
