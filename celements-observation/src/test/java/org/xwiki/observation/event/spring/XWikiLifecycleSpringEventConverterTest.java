bd00e2176a9d8f21f27cb0a44de56e0b5485de59
Marc Sladek <marc@sladek.me>
2023-09-06 11:57:48 +0200

    XWikiLifecycleSpringEventConverter fix (#229)
    * fix XWikiLifecycleSpringEventConverter
    
    * shorten log msg
    
    * remove AbstractComponentTestCase

---
+++ celements-observation/src/test/java/org/xwiki/observation/event/spring/XWikiLifecycleSpringEventConverterTest.java
@@ -0,0 +1,49 @@
+package org.xwiki.observation.event.spring;
+
+import static org.easymock.EasyMock.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.xwiki.observation.ObservationManager;
+import org.xwiki.observation.event.ApplicationStartedEvent;
+import org.xwiki.observation.event.ApplicationStoppedEvent;
+
+import com.celements.init.CelementsLifecycleEvent;
+import com.celements.init.CelementsStartedEvent;
+import com.celements.init.CelementsStoppedEvent;
+
+public class XWikiLifecycleSpringEventConverterTest {
+
+  XWikiLifecycleSpringEventConverter converter;
+  ObservationManager observationManagerMock;
+
+  @Before
+  public void prepare() throws Exception {
+    observationManagerMock = createMock(ObservationManager.class);
+    converter = new XWikiLifecycleSpringEventConverter(observationManagerMock);
+  }
+
+  @Test
+  public void test_CelementsStartedEvent() {
+    observationManagerMock.notify(anyObject(ApplicationStartedEvent.class), same(converter));
+    replay(observationManagerMock);
+    converter.onApplicationEvent(new CelementsStartedEvent(this));
+    verify(observationManagerMock);
+  }
+
+  @Test
+  public void test_CelementsStoppedEvent() {
+    observationManagerMock.notify(anyObject(ApplicationStoppedEvent.class), same(converter));
+    replay(observationManagerMock);
+    converter.onApplicationEvent(new CelementsStoppedEvent(this));
+    verify(observationManagerMock);
+  }
+
+  @Test
+  public void test_other() {
+    replay(observationManagerMock);
+    converter.onApplicationEvent(new CelementsLifecycleEvent(this, "other"));
+    verify(observationManagerMock);
+  }
+
+}
--- celements-servlet/src/main/java/com/celements/init/CelementsLifecycleEvent.java
+++ celements-servlet/src/main/java/com/celements/init/CelementsLifecycleEvent.java
@@ -25,4 +25,9 @@ public class CelementsLifecycleEvent extends ApplicationEvent {
     return state;
   }
 
+  @Override
+  public String toString() {
+    return getState();
+  }
+
 }
