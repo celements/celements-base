a117faa17e46e542e92b2dfdcd6d340a154e6d42
Marc Sladek <marc@sladek.dev>
2025-07-28 14:43:13 +0200

    add file action

--- celements-xwiki-core/src/main/java/com/xpn/xwiki/user/impl/xwiki/XWikiRightServiceImpl.java
+++ celements-xwiki-core/src/main/java/com/xpn/xwiki/user/impl/xwiki/XWikiRightServiceImpl.java
@@ -76,6 +76,7 @@ public class XWikiRightServiceImpl implements XWikiRightService {
       .put("loginsubmit", "login")
       .put("view", "view")
       .put("edit", "edit")
+      .put("file", "view")
       .put("viewrev", "view")
       .put("get", "view")
       .put("downloadrev", "view")
