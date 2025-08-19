b8abfa3b020aa82ce4ed67931158800711bc9db7
Fabian Pichler <fabian.pichler@synventis.com>
2025-08-19 05:32:23 +0200

    add unit test

--- celements-wiki-manager/component/src/test/java/com/celements/wiki/service/WikiManagerServiceTest.java
+++ celements-wiki-manager/component/src/test/java/com/celements/wiki/service/WikiManagerServiceTest.java
@@ -1,18 +1,24 @@
 package com.celements.wiki.service;
 
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.WikiReference;
 
 import com.celements.common.test.AbstractComponentTest;
+import com.celements.model.util.ModelUtils;
+import com.xpn.xwiki.XWikiConstant;
 
 public class WikiManagerServiceTest extends AbstractComponentTest {
 
   private WikiManagerService service;
 
   @Before
-  public void setUp() throws Exception {
+  public void prepare() throws Exception {
+    registerComponentMock(ModelUtils.class);
     service = getSpringContext().getBean(WikiManagerService.class);
   }
 
@@ -21,6 +27,17 @@ public class WikiManagerServiceTest extends AbstractComponentTest {
     replayDefault();
     assertNotNull(service);
     verifyDefault();
+  }
+
+  @Test
+  public void test_getWikiConfigDocRef_mainWiki() {
+    expect(getMock(ModelUtils.class).getMainWikiRef()).andReturn(new WikiReference("unkownMain"));
+    expect(getMock(ModelUtils.class).getDatabaseNameWithoutPrefix(eq(XWikiConstant.MAIN_WIKI)))
+        .andReturn("main");
+    replayDefault();
+    DocumentReference configDocRef = service.getWikiConfigDocRef(XWikiConstant.MAIN_WIKI);
+    verifyDefault();
+    assertEquals("XWikiServerMain", configDocRef.getName());
   }
 
 }
