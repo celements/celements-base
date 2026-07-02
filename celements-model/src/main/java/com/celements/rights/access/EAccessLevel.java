bbb25c3fd4b0b20d9578e816d93ea382f0e843b7
Marc Sladek <marc@sladek.dev>
2026-07-02 22:05:39 +0200

    add EAccessLevel.ADMIN

--- celements-model/src/main/java/com/celements/rights/access/EAccessLevel.java
+++ celements-model/src/main/java/com/celements/rights/access/EAccessLevel.java
@@ -3,20 +3,23 @@ package com.celements.rights.access;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
-import com.celements.common.ReverseMap;
 import com.celements.common.ValueGetter;
 import com.google.common.base.Optional;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.EnumHashBiMap;
 
 public enum EAccessLevel implements ValueGetter<String> {
 
   VIEW("view"), COMMENT("comment"), EDIT("edit"), DELETE("delete"), UNDELETE("undelete"), REGISTER(
-      "register"), PROGRAMMING("programming");
+      "register"), PROGRAMMING("programming"), ADMIN("admin");
 
-  private static ReverseMap<EAccessLevel, String> ID_MAP = new ReverseMap<>(EAccessLevel.values());
+  private static BiMap<String, EAccessLevel> idMap = EnumHashBiMap
+      .<EAccessLevel, String>create(EAccessLevel.class)
+      .inverse();
 
   private final String identifier;
 
-  private EAccessLevel(String identifier) {
+  EAccessLevel(String identifier) {
     this.identifier = identifier;
   }
 
@@ -27,7 +30,7 @@ public enum EAccessLevel implements ValueGetter<String> {
 
   @NotNull
   public static Optional<EAccessLevel> getAccessLevel(@Nullable String identifier) {
-    return ID_MAP.get(identifier);
+    return Optional.fromNullable(idMap.get(identifier));
   }
 
   @Override
